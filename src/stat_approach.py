a4a781734126ac7eca474c74bc1078498f43f8bb
Clement <clement.leveque@ensiie.fr>
2024-11-01 16:41:42 +0100

    renamed statistics to avoid issues with the module

---
+++ src/stat_approach.py
@@ -0,0 +1,10 @@
+### THIS IS A FILE TO EXPLORE AND MAKE VISUALIZATIONS ON THE SIMPLE DATASET
+
+# libraries import
+import requests
+import pandas as pd
+import seaborn as sns
+import librosa
+import numpy as np
+import io
+
