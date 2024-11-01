a19562c24e5c5f4867fbf2d5cb29b2ebc5ef9ba3
Clement <clement.leveque@ensiie.fr>
2024-11-01 12:35:12 +0100

    renamed main.py to data_set_generation and added doc

---
+++ src/data_set_generation.py
@@ -0,0 +1,33 @@
+### THIS IS THE PY CODE TO GENERATE THE DATA SET
+
+# libraries import
+import requests
+import pandas as pd
+import seaborn as sns
+
+# start of the url to use the api
+base_url = "https://pokeapi.co/api/v2/"
+
+# pokemon class
+class Pokemon :
+    def __init__(self, name) -> None:
+        self.name = name
+
+    def getName(self) -> str:
+        return self.name
+
+    # the correct url used with the api
+    def url(self) -> str:
+        return f"{base_url}/pokemon/{self.getName()}"
+    
+    # the general informations of the pokemon (gathered with the api)
+    def info(self) :
+        result = requests.get(self.url())
+
+        if result.status_code == 200 : # the correct status_code
+            return result.json()
+        else :
+            print(f"Failed to retrieve data : {result.status_code}")
+            return None
+
+pokemon_list = requests.get(f"{base_url}/pokemon?limit=100000&offset=0").json()
