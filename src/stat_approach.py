0c588c6df7e2de0c7bf8837a56a66cb1b385abe8
Clement <clement.leveque@ensiie.fr>
2024-11-01 17:42:20 +0100

    created stats for each type

--- src/stat_approach.py
+++ src/stat_approach.py
@@ -15,18 +15,26 @@ if __name__ == "__main__" :
     
     # stats for each type
 
-    normal_poke = df.loc[(df.type_1 == "normal") | (df.type_2 == "normal")]
-    fire_poke = df.loc[(df.type_1 == "fire") | (df.type_2 == "fire")]
-    water_poke = df.loc[(df.type_1 == "water") | (df.type_2 == "water")]
-    electric_poke = df.loc[(df.type_1 == "electric") | (df.type_2 == "electric")]
-    grass_poke = df.loc[(df.type_1 == "grass") | (df.type_2 == "grass")]
-    normal_poke = df.loc[(df.type_1 == "normal") | (df.type_2 == "normal")]
-    ice_poke = df.loc[(df.type_1 == "ice") | (df.type_2 == "ice")]
-    fighting_poke = df.loc[(df.type_1 == "fighting") | (df.type_2 == "fighting")]
-    poison_poke = df.loc[(df.type_1 == "poison") | (df.type_2 == "poison")]
-    ground_poke = df.loc[(df.type_1 == "ground") | (df.type_2 == "ground")]
-    flying_poke = df.loc[(df.type_1 == "flying") | (df.type_2 == "flying")]
-    psychic_poke = df.loc[(df.type_1 == "psychic") | (df.type_2 == "psychic")]
-    bug_poke = df.loc[(df.type_1 == "bug") | (df.type_2 == "bug")]
-    rock_poke = df.loc[(df.type_1 == "rock") | (df.type_2 == "rock")]
-    ghost_poke = df.loc[(df.type_1 == "ghost") | (df.type_2 == "ghost")]
+    types = ["normal", "fire", "water", "electric", "grass", "ice", "fighting", "poison", "ground", "flying", "psychic", "bug", "rock", "ghost"]
+
+    stat = []
+
+    for type in types :
+        stat.append(
+            {
+                'type' : type,
+                'mean-height' : df.loc[(df.type_1 == type) | (df.type_2 == type)]['height'].mean(),
+                'mean-weight' : df.loc[(df.type_1 == type) | (df.type_2 == type)]['weight'].mean(),
+                'mean-hp' : df.loc[(df.type_1 == type) | (df.type_2 == type)]['hp'].mean(),
+                'mean-attack' : df.loc[(df.type_1 == type) | (df.type_2 == type)]['attack'].mean(),
+                'mean-defense' : df.loc[(df.type_1 == type) | (df.type_2 == type)]['defense'].mean(),
+                'mean-special-attack' : df.loc[(df.type_1 == type) | (df.type_2 == type)]['special-attack'].mean(),
+                'mean-special-defense' : df.loc[(df.type_1 == type) | (df.type_2 == type)]['special-defense'].mean(),
+                'mean-speed' : df.loc[(df.type_1 == type) | (df.type_2 == type)]['speed'].mean()
+            }
+        )
+    
+    type_df = pd.DataFrame(stat)
+    type_df.set_index('type', inplace=True)
+    
+    df.to_csv('data/type_data_1stgen.csv', index=False)
